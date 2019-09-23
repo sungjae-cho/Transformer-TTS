f931389ae2413efe705f91ba1f0b82f8bb74fb2a
Sungjae Cho <sungjae.cho.1118@gmail.com>
2019-09-23 16:02:52 +0900

    Change the path format to make adaptable to checkpoint dir path.

--- synthesis.py
+++ synthesis.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 import argparse
 
 def load_checkpoint(step, model_name="transformer"):
-    state_dict = t.load('./checkpoint/checkpoint_%s_%d.pth.tar'% (model_name, step))   
+    state_dict = t.load('%s/checkpoint_%s_%d.pth.tar'% (hp.checkpoint_path, model_name, step))
     new_state_dict = OrderedDict()
     for k, value in state_dict['model'].items():
         key = k[7:]
@@ -36,7 +36,7 @@ def synthesis(text, args):
     m_post = m_post.cuda()
     m.train(False)
     m_post.train(False)
-    
+
     pbar = tqdm(range(args.max_len))
     with t.no_grad():
         for i in pbar:
@@ -45,12 +45,12 @@ def synthesis(text, args):
             mel_input = t.cat([mel_input, postnet_pred[:,-1:,:]], dim=1)
 
         mag_pred = m_post.forward(postnet_pred)
-        
+
     wav = spectrogram2wav(mag_pred.squeeze(0).cpu().numpy())
     write(hp.sample_path + "/test.wav", hp.sr, wav)
-    
+
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--restore_step1', type=int, help='Global step to restore checkpoint', default=172000)
     parser.add_argument('--restore_step2', type=int, help='Global step to restore checkpoint', default=100000)
